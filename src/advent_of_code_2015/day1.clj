3145e066bdb2517d1e2763d24dd5b2f1aac70fd8
Stephen Meyles <smeyles@localhost.localdomain>
2015-12-11 12:25:25 -0800

    Adding day one solution

---
+++ src/advent_of_code_2015/day1.clj
@@ -0,0 +1,16 @@
+(ns advent-of-code-2015.day1
+  (:require [clojure.java.io :as io]))
+
+(defn run []
+  (let [input (slurp (io/resource "input.txt"))
+        steps (map #(case %1 \( 1 \) -1 0) input)]
+    (println (reduce + steps))
+    (println
+      (:count
+        (reduce
+          #(if (= (:floor %1) -1)
+             (reduced %1)
+             {:floor (+ (:floor %1) %2) :count (inc (:count %1))})
+          {:floor 0 :count 0}
+          steps)))))
+
