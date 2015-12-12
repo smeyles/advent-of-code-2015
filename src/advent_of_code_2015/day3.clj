750e44ee3bb9921e3fb628061484f0d98f39ed09
Stephen Meyles <smeyles@localhost.localdomain>
2015-12-12 14:00:35 -0800

    Used take-nth, simplified calc to set/union

--- src/advent_of_code_2015/day3.clj
+++ src/advent_of_code_2015/day3.clj
@@ -14,27 +14,11 @@
   (let [move (offset c) head (first positions)]
     (conj positions {:x (+ (:x move) (:x head)) :y (+ (:y move) (:y head))})))
 
-; bah - couldn't find/discover take-nth
-(defn uninterleave-n
-  ([coll]
-    (uninterleave-n coll 2 0))
-  ([coll freq]
-    (uninterleave-n coll freq 0))
-  ([coll freq offset]
-    (lazy-seq
-      (when-let [s (seq coll)]
-        (let [f (first s) r (rest s) modinc #(mod (inc %) freq)]
-          (if (= 0 (mod offset freq))
-            (cons f (uninterleave-n r freq (modinc offset)))
-            (uninterleave-n r freq (modinc offset))))))))
-
-(defn run
-  ([] (run "day3.txt")) 
-  ([filename]
-   (let [input (slurp (io/resource filename))
-         houses #(into #{} (reduce deliver' '({:x 0 :y 0}) %))]
-     (count (houses input))
-     (let [santa (houses (uninterleave-n input))
-           robo (houses (uninterleave-n (rest input)))]
-       (- (+ (count santa) (count robo)) (count (set/intersection santa robo)))))))
+(defn run []
+  (let [input (slurp (io/resource "day3.txt"))
+        houses #(into #{} (reduce deliver' '({:x 0 :y 0}) %))]
+    (count (houses input))
+    (let [santa (houses (take-nth 2 input))
+          robo (houses (take-nth 2 (rest input)))]
+      (count (set/union santa robo)))))
 
