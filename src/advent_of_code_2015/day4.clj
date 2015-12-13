4b48a0a42f7a69b93f8fd695d7195a08af4930d9
Stephen Meyles <smeyles@gmail>
2015-12-12 16:22:55 -0800

    Rewrote with list comprehension rather than tail recursion

--- src/advent_of_code_2015/day4.clj
+++ src/advent_of_code_2015/day4.clj
@@ -6,11 +6,11 @@
   (.toString (BigInteger. 1 (.digest algo)) 16))
 
 (defn run
-  ([] (run "yzbqklnj" 0 26))
-  ([prefix n len]
+  ([] (run "yzbqklnj" 26))
+  ([prefix len]
    (let [md5 (MessageDigest/getInstance "MD5")
-         hash (digest md5 (format "%s%d" prefix n))]
-     (if (= len (count hash))
-       n
-       (recur prefix (inc n) len)))))
-
+         hash #(digest md5 (format "%s%d" prefix %))]
+     (first
+       (for [n (range) :let [hash (digest md5 (format "%s%d" prefix n))]
+                       :when (= len (count hash))]
+         n))))) 
