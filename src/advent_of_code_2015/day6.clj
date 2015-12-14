f8c7f21ff4244eef87c14a074df8c98ff02c7512
Stephen Meyles <smeyles@gmail>
2015-12-13 22:15:57 -0800

    Updating day six to use re-seq

--- src/advent_of_code_2015/day6.clj
+++ src/advent_of_code_2015/day6.clj
@@ -4,42 +4,31 @@
 
 (def re-parse #"(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)")
 
-(defn parse [l]
-  (let [matcher (re-matcher re-parse l)]
-    (if (re-find matcher)
-      (let [g (re-groups matcher)]
-        (conj (map #(Integer/parseInt %) (drop 2 g)) (get g 1)))
-      nil)))
-
 (defn switch [instr grid [x y]]
   (let [n (+ x (* 1000 y))]
-  (case instr
-    "turn on" (assoc! grid n 1)
-    "turn off" (assoc! grid n 0)
-    "toggle" (assoc! grid n (- 1 (get grid n))))))
+    (case instr
+      "turn on" (assoc! grid n 1)
+      "turn off" (assoc! grid n 0)
+      "toggle" (assoc! grid n (- 1 (get grid n))))))
 
 (defn switch' [instr grid [x y]]
   (let [n (+ x (* 1000 y))]
-  (case instr
-    "turn on" (assoc! grid n (inc (get grid n)))
-    "turn off" (assoc! grid n (max 0 (dec (get grid n))))
-    "toggle" (assoc! grid n (+ 2 (get grid n))))))
+    (case instr
+      "turn on" (assoc! grid n (inc (get grid n)))
+      "turn off" (assoc! grid n (max 0 (dec (get grid n))))
+      "toggle" (assoc! grid n (+ 2 (get grid n))))))
 
-(defn light [s grid [instr x1 y1 x2 y2]]
-  (let [lights (for [x (range x1 (inc x2)) y (range y1 (inc y2))] [x y])]
+(defn light [s grid [_ instr x1 y1 x2 y2]]
+  (let [p #(Integer/parseInt %)
+        lights (for [x (range (p x1) (inc (p x2)))
+                     y (range (p y1) (inc (p y2)))] [x y])]
     (reduce (partial s instr) grid lights)))
 
 (defn run []
-  (with-open [rdr (io/reader (io/resource "day6.txt"))]
-    (let [grid (into [] (repeat 1000000 0))]
-      ;(->> (line-seq rdr)
-           ;(map parse)
-           ;(reduce (partial light switch) (transient grid))
-           ;(persistent!)
-           ;(reduce +))
-      (->> (line-seq rdr)
-           (map parse)
-           (reduce (partial light switch') (transient grid))
-           (persistent!)
-           (reduce +)))))
+  (let [grid (into [] (repeat 1000000 0))]
+    (->> (slurp (io/resource "day6.txt"))
+         (re-seq re-parse)
+         (reduce (partial light switch') (transient grid))
+         (persistent!)
+         (reduce +))))
 
